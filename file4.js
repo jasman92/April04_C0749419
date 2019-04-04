fee410584779c19cdb8c7b72c1b9d99fb1c4ece9
Manpreet Pal Kaur <749419@student.tor.loc>
2019-04-04 10:56:46 -0400

    nextn

---
+++ file4.js
@@ -0,0 +1,10 @@
+var user ={
+    first_name:"css",
+    last_name: "HTML",
+    age:4, /* from www.java2s.com */
+    website: "java2s.com"
+};
+
+for (key in user) {
+    console.log(key.value);
+}
